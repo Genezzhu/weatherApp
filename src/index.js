c1621ba9a61028fb7daa6db133ba2a7607b701a0
Gene Zhu <gene.gn.zhu@gmail.com>
2021-08-02 13:07:15 +1000

    finalised

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import dva from 'dva';
+import createLoading from 'dva-loading'
+import { browserHistory } from 'dva/router';
+import weather from '@/models/weather';
+import routerConf from '@/router';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = dva({
+  history: browserHistory
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(createLoading());
+
+app.model(weather);
+
+app.router(routerConf);
+
+app.start('#root');
